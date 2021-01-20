772aa0269b6ee6155f85d7c40aaa99f77b94d8f8
Ricardo Chavarria <jr.chavarria21@gmail.com>
2021-01-20 08:08:34 -0600

    Add Roboto Font

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './pages/home/home.component';
+import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'speakers',
+    component: SpeakersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SpeakersComponent } from './pages/speakers/speakers.component';
+import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroHeaderComponent } from './components/hero-header/hero-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SpeakersComponent, SponsorsComponent, HomeComponent, HeroHeaderComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
